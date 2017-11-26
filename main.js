5aad75518fb9d2ba5a91db2a7d957bb32f004421
parklh <parklisah@gmail.com>
2017-11-26 13:42:42 -0500

    completing the scafollding

---
+++ main.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicGFya2xoIiwiYSI6ImNqOWkwYnJrczNmYWgycXM0dnFxM2xrMmsifQ.ht37-qzhJuttsSImu7PXEA';
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v9'
+        });
